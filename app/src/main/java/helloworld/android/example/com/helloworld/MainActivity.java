e836827e0aa0de7df7e75c5066de8a4055439dcd
Mostafa Aswany <33361106+MostafaAswany@users.noreply.github.com>
2018-01-28 01:33:59 +0200

    Hello World

---
+++ app/src/main/java/helloworld/android/example/com/helloworld/MainActivity.java
@@ -0,0 +1,28 @@
+package helloworld.android.example.com.helloworld;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView img = (ImageView)findViewById(R.id.url_image);
+        img.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("https://www.udacity.com"));
+                startActivity(intent);
+            }
+        });
+    }
+}
